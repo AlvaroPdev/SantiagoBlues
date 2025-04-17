005ad0c32381771fe58ef42eb562f4f93af33a5d
Alvaro <ampcastro29@gmail.com>
2025-04-17 17:43:45 -0400

    Estructura base del proyecto Santiago Blues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { AgendarComponent } from './pages/agendar/agendar.component';
+import { AdminComponent } from './pages/admin/admin.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'agendar', component: AgendarComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: '**', redirectTo: '' },
+];
